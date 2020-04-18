4c8e3941948b4a344b4a90002f0247e877e6cd99
tetsu5555 <tetete1118@gmail.com>
2020-04-18 15:54:30 +0900

    chore: rename folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     popup: "./popup.js"
   },
   output: {
-    path: path.resolve(__dirname, 'app'),
+    path: path.resolve(__dirname, 'dist'),
     filename: "[name].js"
   }
 }
