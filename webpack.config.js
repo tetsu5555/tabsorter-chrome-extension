d26b3fe6e8763cd2606b485bd4dcbcee574c01ab
tetsu5555 <tetete1118@gmail.com>
2020-04-18 16:32:56 +0900

    chore: clean files

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  context: __dirname + '/src',
   entry: {
-    content: "./content.js",
-    background: "./background.js",
-    popup: "./popup.js"
+    content: "./content/content.js",
+    background: "./background/background.js",
+    popup: "./popup/popup.js"
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
