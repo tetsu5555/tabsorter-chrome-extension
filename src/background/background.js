362323ff7b48287e7e672a4a3c593b91b12c92da
tetsu5555 <tetete1118@gmail.com>
2020-04-18 19:53:35 +0900

    test

--- src/background/background.js
+++ src/background/background.js
@@ -1,1 +1,38 @@
-alert("background is loaded");
+// 拡張機能がインストールされたときの処理
+chrome.runtime.onInstalled.addListener(function(){
+
+  // 親階層のメニューを生成
+  const parent_menu = chrome.contextMenus.create({
+    type: "normal",
+    id: "parent",
+    title: "背景色を変えるメニュー"
+  });
+
+  //子階層のメニューを親(parent_menu)に追加
+  chrome.contextMenus.create({
+    id: "red",
+    parentId: parent_menu,
+    title: "赤色"
+  });
+
+  chrome.contextMenus.create({
+    id: "blue",
+    parentId: parent_menu,
+    title: "青色"
+  });
+});
+
+
+/**
+  * メニューが選択されたときの処理
+  * 選択されたアイテムはこちらの関数の引数に入ってくる(今回は item)
+  * menuItemIdでmenuのidが文字列で取得できる
+  */
+chrome.contextMenus.onClicked.addListener(function(item){
+  console.log("メニューがクリックされたよ");
+
+  // 選ばれたメニューのidが item.menuItemId で取得できる
+  chrome.tabs.executeScript({
+    code: "document.body.style.backgroundColor = '" + item.menuItemId + "'"
+  });
+});
