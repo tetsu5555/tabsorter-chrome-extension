0f6517295c41266b6bb4e0aead918741e22c71b2
tetsu5555 <tetete1118@gmail.com>
2020-04-18 16:48:40 +0900

    chore: add webpack CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  context: __dirname + '/src',
+  mode: process.env.NODE_ENV,
+  context: path.resolve(__dirname, "src"),
   entry: {
-    content: "./content/content.js",
-    background: "./background/background.js",
-    popup: "./popup/popup.js"
+    "content/content": "./content/content.js",
+    "background/background": "./background/background.js",
+    "popup/popup": "./popup/popup.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     filename: "[name].js"
-  }
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: "assets", to: "assets" },
+      { from: "./popup/popup.html", to: "popup" },
+      { from: "./manifest.json", to: "" },
+    ]),
+  ],
 }
